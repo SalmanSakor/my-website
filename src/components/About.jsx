dd414dd081aa3265fb33de26df0409363f190399
Salman Sakor <146375700+SalmanSakor@users.noreply.github.com>
2024-03-28 21:36:45 +0200

    my-website

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,20 +10,19 @@ const About = () => {
               As a dedicated front-end web developer, I bring a year of hands-on
               experience in the dynamic realm of web development, Armed with a
               bachelor's degree in information and communications technology
-              engineering, I possess a solid foundation for crafting innovative
-              digital solutions.
+              engineering.
             </p>
             <p>
               My expertise spans HTML, CSS, SASS, and JavaScript, complemented
-              by proficiency in frameworks like Bootstrap and React JS, I am
-              adept at working with Figma files and leveraging libraries such as
-              Redux, Redux Toolkit, Thunk, and Saga to enhance functionality.
+              by proficiency in frameworks like Bootstrap and React JS.
+              <br />I am adept at working with Figma files and leveraging
+              libraries such as Redux, Redux Toolkit, Thunk, and Saga to enhance
+              functionality.
             </p>
             <p>
-              Known for my meticulous approach, I specialize in delivering
-              intuitive and user-friendly web applications, with a fervent
-              enthusiasm for creating exceptional web experiences, I thrive in
-              the fast-paced and ever-evolving landscape of technology.
+              I specialize in delivering intuitive and user-friendly web
+              applications, with a fervent enthusiasm for creating exceptional
+              web experiences.
             </p>
           </div>
         </div>
