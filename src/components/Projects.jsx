74b6eb3f0a5d3fda8046f090b047f4f5b05b3215
Salman Sakor <146375700+SalmanSakor@users.noreply.github.com>
2024-04-12 00:25:25 +0300

    salman

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,17 @@ const Projects = () => {
     <div className="container">
       <div class="card">
         <h2 className="card-h2">My Projects</h2>
+
+        <div style={{ textAlign: "center" }}>
+          <h2>e-commerce / front-end : react / back-end : laravel</h2>
+        </div>
+
+        <h4>admin account : admin@gmail.com / password : 88888888</h4>
+        <h4>
+          manger account : products-manger@gmail.com / password : 88888888
+        </h4>
       </div>
+
       <div className="items">
         <p>source code and more projects in</p>
 
